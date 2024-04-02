025620b2060e23333c61d5326f577a9e714771f9
Yoonjae Choi <dbswo9795@gmail.com>
2024-04-03 00:29:50 +0900

    style: 색상, 폰트 사이즈 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,38 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        gray100: '#f8f9fa',
+        gray200: '#e9ecef',
+        gray300: '#dee2e6',
+        gray400: '#ced4da',
+        gray500: '#adb5bd',
+        gray600: '#6c757d',
+        gray700: '#495057',
+        gray800: '#343a40',
+        gray900: '#212529',
+        yellow: '#ffbe0b',
+        orange: '#fb5607',
+        red: '#ff006e',
+        purple: '#8338ec',
+        blue: '#3a86ff',
+      },
+      fontSize: {
+        '14': '14px',
+        '16': '16px',
+        '18': '18px',
+        '20': '20px',
+        '24': '24px',
+        '28': '28px',
+        '32': '32px',
+        '40': '40px',
       },
     },
   },
