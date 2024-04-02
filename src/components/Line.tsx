7be2e76715cac078ccff17c0cbe59ac31422b73a
Yoonjae Choi <dbswo9795@gmail.com>
2024-04-03 00:37:51 +0900

    design: 라인 컴포넌트 수정

--- src/components/Line.tsx
+++ src/components/Line.tsx
@@ -1,20 +1,19 @@
 import { HTMLAttributes } from 'react';
 
-import { cn } from '@/utils';
-
 interface LineProps extends HTMLAttributes<HTMLHRElement> {
-  border: number;
-  color: string;
+  border?: number;
+  color?: string;
 }
 
 export const Line = ({ border, color }: LineProps) => {
   return (
     <hr
-      className={
-        (cn('w-full border border-gray200'),
-        `border-[${border}px]`,
-        `border-${color}`)
-      }
+      style={{
+        width: '100%',
+        borderBottom: `${border ? border : '1'}px solid ${
+          color ? color : '#dee2e6'
+        }`,
+      }}
     />
   );
 };
