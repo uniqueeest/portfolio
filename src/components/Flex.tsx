9d3ed01e7fbd10423c016ee8aeca076274361521
Yoonjae Choi <dbswo9795@gmail.com>
2024-04-14 00:58:20 +0900

    design: 클래스 타입으로 수정

--- src/components/Flex.tsx
+++ src/components/Flex.tsx
@@ -1,3 +1,29 @@
+import { cn } from '@/utils';
+
+const flexDirectionClass: Record<string, string> = {
+  row: 'flex-row',
+  column: 'flex-col',
+  'row-reverse': 'flex-row-reverse',
+  'column-reverse': 'flex-col-reverse',
+};
+
+const alignItemsClass: Record<string, string> = {
+  start: 'items-start',
+  end: 'items-end',
+  center: 'items-center',
+  baseline: 'items-baseline',
+  stretch: 'items-stretch',
+};
+
+const justifyContentClass: Record<string, string> = {
+  start: 'justify-start',
+  end: 'justify-end',
+  center: 'justify-center',
+  between: 'justify-between',
+  around: 'justify-around',
+  evenly: 'justify-evenly',
+};
+
 interface FlexProps {
   align?: string;
   justify?: string;
@@ -15,16 +41,20 @@ export const Flex = ({
   className,
   children,
 }: FlexProps) => {
+  const flexClasses = cn(
+    'flex',
+    flexDirectionClass[direction],
+    alignItemsClass[align],
+    justifyContentClass[justify],
+    className
+  );
+
   return (
     <div
       style={{
-        display: 'flex',
-        flexDirection: direction,
-        alignItems: align,
-        justifyContent: justify,
         gap: gap && `${gap}px`,
       }}
-      className={className}
+      className={flexClasses}
     >
       {children}
     </div>
