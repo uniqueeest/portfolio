b172b977fea4476fe544f8c604f589c61ac456a4
Yoonjae Choi <dbswo9795@gmail.com>
2024-04-14 00:58:08 +0900

    design: 미디어 쿼리 분기 추가

--- src/constants/style.ts
+++ src/constants/style.ts
@@ -1,3 +1,4 @@
 export const LINK_STYLE =
   'hover:text-blue dark:text-gray100 dark:hover:text-blue';
-export const LIST_STYLE = 'flex flex-col gap-3 list-disc dark:text-gray100';
+export const LIST_STYLE =
+  'flex flex-col gap-3 list-disc dark:text-gray100 sm:list-none';
