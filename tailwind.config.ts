016cff58fdc50eaad544b2aa1d517395f31fdf95
Yoonjae Choi <dbswo9795@gmail.com>
2024-04-14 00:57:53 +0900

    style: 미디어 쿼리 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,13 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    screens: {
+      '2xl': { max: '1535px' },
+      xl: { max: '1279px' },
+      lg: { max: '1023px' },
+      md: { max: '767px' },
+      sm: { max: '639px' },
+    },
     extend: {
       colors: {
         gray100: '#f8f9fa',
